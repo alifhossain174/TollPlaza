e581fa1ffbce0757785d49c60bd4d91500f670a5
Fahim <alifhossain174@gmail.com>
2025-02-02 05:54:38 +0100

    Updating CVode

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TollTicket;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 
@@ -28,7 +31,25 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('backend.dashboard');
+        if(Auth::user()->user_type == 1){
+            $tollTickets = TollTicket::orderBy('id', 'desc')->skip(0)->limit(6)->get();
+            $vehicleCounts = DB::table('toll_tickets')
+                            ->select('vehicle_type_id', 'vehicle_type_name', DB::raw('COUNT(*) as total'))
+                            ->groupBy('vehicle_type_id')
+                            ->orderByDesc('total') // Order by count in descending order
+                            ->limit(6) // Take only 6 rows
+                            ->get();
+        } else {
+            $tollTickets = TollTicket::where('user_id', Auth::user()->id)->orderBy('id', 'desc')->skip(6)->limit(10)->get();
+            $vehicleCounts = DB::table('toll_tickets')
+                            ->select('vehicle_type_id', 'vehicle_type_name', DB::raw('COUNT(*) as total'))
+                            ->groupBy('vehicle_type_id')
+                            ->where('user_id', Auth::user()->id)
+                            ->orderByDesc('total') // Order by count in descending order
+                            ->limit(6) // Take only 6 rows
+                            ->get();
+        }
+        return view('backend.dashboard', compact('tollTickets', 'vehicleCounts'));
     }
 
     public function changePasswordPage(){
--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -38,121 +38,175 @@
 @section('content')
 
     <div class="row">
+
+        @if(Auth::user()->user_type == 1)
         <div class="col-lg-6 col-xl-3">
             <div class="card graph_card">
                 <div class="card-body">
                     <div class="row align-items-center">
                         <div class="col">
-                            <h6 class="text-uppercase font-size-12 text-muted mb-3">No Of Orders (Monthly)</h6>
-                            <span class="h3 mb-0"> 10 </span>
+                            <h6 class="text-uppercase font-size-12 text-muted mb-3">No Of Active Terminals</h6>
+                            <span class="h3 mb-0"> {{DB::table('terminals')->where('status', 1)->count()}} </span>
                         </div>
                     </div> <!-- end row -->
 
                     <div id="sparkline1" class="mt-3"></div>
                 </div> <!-- end card-body-->
-                <i class="feather-shopping-cart" style="color: #0074E4; background: #0074E42E;"></i>
+                <i class="feather-toggle-right " style="color: #0074E4; background: #0074E42E;"></i>
             </div> <!-- end card-->
         </div> <!-- end col-->
-
+        @else
         <div class="col-lg-6 col-xl-3">
             <div class="card graph_card">
                 <div class="card-body">
                     <div class="row align-items-center">
                         <div class="col">
-                            <h6 class="text-uppercase font-size-12 text-muted mb-3">Total Revenue (Monthly)</h6>
-                            <span class="h3 mb-0"> ৳ 2500 </span>
+                            <h6 class="text-uppercase font-size-12 text-muted mb-3">My Terminal</h6>
+                            <span class="h5 mb-0"> {{DB::table('terminals')->where('id', Auth::user()->terminal_id)->first()->name}} </span>
+                        </div>
+                    </div> <!-- end row -->
+
+                    <div id="sparkline1" class="mt-3"></div>
+                </div> <!-- end card-body-->
+                <i class="feather-toggle-right " style="color: #0074E4; background: #0074E42E;"></i>
+            </div> <!-- end card-->
+        </div>
+        @endif
+
+        @if(Auth::user()->user_type == 1)
+        <div class="col-lg-6 col-xl-3">
+            <div class="card graph_card">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <h6 class="text-uppercase font-size-12 text-muted mb-3">Total Active Operators</h6>
+                            <span class="h3 mb-0"> {{DB::table('users')->where('status', 1)->where('user_type', 2)->count()}} </span>
                         </div>
                     </div> <!-- end row -->
                     <div id="sparkline2" class="mt-3"></div>
                 </div> <!-- end card-body-->
-                <i class="feather-trending-up" style="color: #17263A; background: #17263A3D;"></i>
+                <i class="feather-users" style="color: #17263A; background: #17263A3D;"></i>
             </div> <!-- end card-->
         </div> <!-- end col-->
-
+        @else
         <div class="col-lg-6 col-xl-3">
             <div class="card graph_card">
                 <div class="card-body">
                     <div class="row align-items-center">
                         <div class="col">
-                            <h6 class="text-uppercase font-size-12 text-muted mb-3">Todays Order (Daily)</h6>
-                            <span class="h3 mb-0"> 25 </span>
-                            <span class="h3 mb-0"> <a target="_blank" href="{{url('view/pending/orders')}}" style="height: 20px; line-height: 10px; margin-top: -4px;" class="btn btn-sm btn-success rounded">View All</a> </span>
+                            <h6 class="text-uppercase font-size-12 text-muted mb-3">Operator Name</h6>
+                            <span class="h5 mb-0"> {{Auth::user()->name}} </span>
+                        </div>
+                    </div> <!-- end row -->
+                    <div id="sparkline2" class="mt-3"></div>
+                </div> <!-- end card-body-->
+                <i class="feather-users" style="color: #17263A; background: #17263A3D;"></i>
+            </div> <!-- end card-->
+        </div> <!-- end col-->
+        @endif
+
+        @if(Auth::user()->user_type == 1)
+        <div class="col-lg-6 col-xl-3">
+            <div class="card graph_card">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <h6 class="text-uppercase font-size-12 text-muted mb-3">Todays Ticket Collection</h6>
+                            <span class="h3 mb-0"> {{number_format(DB::table('toll_tickets')->where('created_at', 'LIKE', date("Y-m-d")."%")->sum('ticket_price'))}}/= </span>
                         </div>
                     </div> <!-- end row -->
 
                     <div id="sparkline3" class="mt-3"></div>
                 </div> <!-- end card-body-->
-                <i class="feather-package" style="color: #c28a00; background: #daa5202e;"></i>
+                <i class="feather-trending-up" style="color: #c28a00; background: #daa5202e;"></i>
             </div> <!-- end card-->
         </div> <!-- end col-->
-
+        @else
         <div class="col-lg-6 col-xl-3">
             <div class="card graph_card">
                 <div class="card-body">
                     <div class="row align-items-center">
                         <div class="col">
-                            <h6 class="text-uppercase font-size-12 text-muted mb-3">Registered Users (Monthly)</h6>
-                            <span class="h3 mb-0"> 125 </span>
+                            <h6 class="text-uppercase font-size-12 text-muted mb-3">Todays Ticket Collection</h6>
+                            <span class="h3 mb-0"> {{number_format(DB::table('toll_tickets')->where('terminal_id', Auth::user()->terminal_id)->where('created_at', 'LIKE', date("Y-m-d")."%")->sum('ticket_price'))}}/= </span>
+                        </div>
+                    </div> <!-- end row -->
+
+                    <div id="sparkline3" class="mt-3"></div>
+                </div> <!-- end card-body-->
+                <i class="feather-trending-up" style="color: #c28a00; background: #daa5202e;"></i>
+            </div> <!-- end card-->
+        </div>
+        @endif
+
+        @if(Auth::user()->user_type == 1)
+        <div class="col-lg-6 col-xl-3">
+            <div class="card graph_card">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <h6 class="text-uppercase font-size-12 text-muted mb-3">Total Ticket Collection</h6>
+                            <span class="h3 mb-0"> {{number_format(DB::table('toll_tickets')->sum('ticket_price'))}}/= </span>
                         </div>
                     </div> <!-- end row -->
 
                     <div id="sparkline4" class="mt-3"></div>
                 </div> <!-- end card-body-->
-                <i class="feather-users" style="color: #a60000; background: #a6000026;"></i>
+                <i class="feather-trending-up" style="color: #a60000; background: #a6000026;"></i>
             </div> <!-- end card-->
         </div> <!-- end col-->
-    </div>
-    <!-- end row-->
-
-    <div class="row">
-        <div class="col-xl-4 col-lg-5">
-            <div class="card">
+        @else
+        <div class="col-lg-6 col-xl-3">
+            <div class="card graph_card">
                 <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <h6 class="text-uppercase font-size-12 text-muted mb-3">Total Ticket Collection</h6>
+                            <span class="h3 mb-0"> {{number_format(DB::table('toll_tickets')->where('terminal_id', Auth::user()->terminal_id)->sum('ticket_price'))}}/= </span>
+                        </div>
+                    </div> <!-- end row -->
 
-                    <h4 class="card-title">Best Selling Products (6 months)</h4>
-                    <p class="card-subtitle mb-4">From date of {{date("jS M, Y")}} to continue</p>
-
-                    <div id="morris-donut-example" class="morris-chart"></div>
-
+                    <div id="sparkline4" class="mt-3"></div>
                 </div> <!-- end card-body-->
+                <i class="feather-trending-up" style="color: #a60000; background: #a6000026;"></i>
             </div> <!-- end card-->
-
-        </div> <!-- end col-->
-
-        <div class="col-xl-8 col-lg-7">
-            <div class="card">
-                <div class="card-body">
-                    <h4 class="card-title">Sales Analytics (Successfull & Failed Order Ratio)</h4>
-                    <p class="card-subtitle mb-4" style="color: trans">From Last Few Months</p>
-                    <div id="morris-bar-example" class="morris-chart"></div>
-                </div>
-            </div>
-        </div> <!-- end col-->
+        </div>
+        @endif
     </div>
     <!-- end row-->
 
     <div class="row">
-        <div class="col-xl-6">
+        <div class="col-xl-7">
             <div class="card">
                 <div class="card-body" style="min-height: 435px;">
 
-                    <h4 class="card-title">Recent Customers</h4>
+                    <h4 class="card-title">Recent Toll Tickets</h4>
 
                     <div class="table-responsive">
                         <table class="table table-centered table-striped table-nowrap">
                             <thead>
                                 <tr>
-                                    <th>Customer</th>
-                                    <th>Phone</th>
-                                    <th>Email</th>
-                                    <th>Location</th>
-                                    <th>Create Date</th>
+                                    <th class="text-center">Vehicle Type</th>
+                                    <th class="text-center">Entered At</th>
+                                    <th class="text-center">Terminal</th>
+                                    <th class="text-center">Price</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                @if(count($tollTickets) > 0)
+                                    @foreach ($tollTickets as $tollTicket)
+                                    <tr>
+                                        <td class="text-center">{{$tollTicket->vehicle_type_name}}</td>
+                                        <td class="text-center">{{date("Y-m-d h:i:s a", strtotime($tollTicket->created_at))}}</td>
+                                        <td class="text-center">{{$tollTicket->terminal_name}}</td>
+                                        <td class="text-center">{{$tollTicket->ticket_price}}/=</td>
+                                    </tr>
+                                    @endforeach
+                                @else
                                 <tr>
-                                    <td colspan="5" class="text-center">No Customers Found</td>
+                                    <td colspan="4" class="text-center">No Data Found</td>
                                 </tr>
+                                @endif
                             </tbody>
                         </table>
                     </div>
@@ -161,26 +215,33 @@
             </div> <!-- end card-->
         </div> <!-- end col -->
 
-        <div class="col-xl-6">
+        <div class="col-xl-5">
             <div class="card">
                 <div class="card-body" style="min-height: 435px;">
 
-                    <h4 class="card-title">Account Transactions</h4>
+                    <h4 class="card-title">Vehicle Typewise Tickets</h4>
 
                     <div class="table-responsive">
                         <table class="table table-centered table-striped table-nowrap">
                             <thead>
                                 <tr>
-                                    <th>TRX ID</th>
-                                    <th>Amount</th>
-                                    <th>Card Type/Brand</th>
-                                    <th>Payment Through</th>
+                                    <th class="text-center">Vehicle Type</th>
+                                    <th class="text-center">No of Tickets</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                @if(count($vehicleCounts) > 0)
+                                    @foreach ($vehicleCounts as $vehicleCount)
+                                    <tr>
+                                        <td class="text-center">{{$vehicleCount->vehicle_type_name}}</td>
+                                        <td class="text-center">{{$vehicleCount->total}}</td>
+                                    </tr>
+                                    @endforeach
+                                @else
                                 <tr>
-                                    <td colspan="4" class="text-center">No Transaction Found</td>
+                                    <td colspan="4" class="text-center">No Data Found</td>
                                 </tr>
+                                @endif
                             </tbody>
                         </table>
                     </div>
@@ -190,180 +251,4 @@
         </div> <!-- end col -->
     </div>
     <!-- end row-->
-@endsection
-
-@section('footer_js')
-    <script>
-        $(document).ready(function() {
-
-            var DrawSparkline = function() {
-                $('#sparkline1').sparkline([10,9,8,7,6,5,4,3,2
-                ], {
-                    type: 'line',
-                    width: "100%",
-                    height: '50',
-                    chartRangeMax: 35,
-                    lineColor: '#2e7ce4',
-                    fillColor: 'rgba(46, 124, 228, 0.3)',
-                    highlightLineColor: 'rgba(0,0,0,.1)',
-                    highlightSpotColor: 'rgba(0,0,0,.2)',
-                    maxSpotColor: false,
-                    minSpotColor: false,
-                    spotColor: false,
-                    lineWidth: 1
-                });
-
-                $('#sparkline2').sparkline([10,9,8,7,6,5,4,3,2
-                ], {
-                    type: 'line',
-                    width: "100%",
-                    height: '50',
-                    chartRangeMax: 35,
-                    lineColor: '#627898',
-                    fillColor: 'rgba(98, 120, 152, 0.3)',
-                    highlightLineColor: 'rgba(0,0,0,.1)',
-                    highlightSpotColor: 'rgba(0,0,0,.2)',
-                    maxSpotColor: false,
-                    minSpotColor: false,
-                    spotColor: false,
-                    lineWidth: 1
-                });
-
-                $('#sparkline3').sparkline([10,9,8,7,6,5,4,3,2
-                ], {
-                    type: 'line',
-                    width: "100%",
-                    height: '50',
-                    chartRangeMax: 35,
-                    lineColor: '#f1bf43',
-                    fillColor: 'rgba(241, 191, 67, 0.3)',
-                    highlightLineColor: 'rgba(0,0,0,.1)',
-                    highlightSpotColor: 'rgba(0,0,0,.2)',
-                    maxSpotColor: false,
-                    minSpotColor: false,
-                    spotColor: false,
-                    lineWidth: 1
-                });
-
-                $('#sparkline4').sparkline([10,9,8,7,6,5,4,3,2
-                ], {
-                    type: 'line',
-                    width: "100%",
-                    height: '50',
-                    chartRangeMax: 35,
-                    lineColor: '#df3554',
-                    fillColor: 'rgba(223, 53, 84, 0.3)',
-                    highlightLineColor: 'rgba(0,0,0,.1)',
-                    highlightSpotColor: 'rgba(0,0,0,.2)',
-                    maxSpotColor: false,
-                    minSpotColor: false,
-                    spotColor: false,
-                    lineWidth: 1
-                });
-            };
-
-            DrawSparkline();
-
-            var resizeChart;
-
-            $(window).resize(function(e) {
-                clearTimeout(resizeChart);
-                resizeChart = setTimeout(function() {
-                    DrawSparkline();
-                }, 300);
-            });
-        });
-
-
-        $(function() {
-            'use strict';
-            if ($("#morris-bar-example").length) {
-                Morris.Bar({
-                    element: 'morris-bar-example',
-                    barColors: ['#00C2B2', '#DF3554'],
-                    data: [
-                        {
-                            y: '10',
-                            a: 2,
-                            b: 8
-                        }
-                        {
-                            y: '10',
-                            a: 2,
-                            b: 8
-                        }
-                        {
-                            y: '10',
-                            a: 2,
-                            b: 8
-                        }
-                        {
-                            y: '10',
-                            a: 2,
-                            b: 8
-                        }
-                        {
-                            y: '10',
-                            a: 2,
-                            b: 8
-                        }
-                        {
-                            y: '10',
-                            a: 2,
-                            b: 8
-                        }
-                        {
-                            y: '10',
-                            a: 2,
-                            b: 8
-                        }
-                        {
-                            y: '10',
-                            a: 2,
-                            b: 8
-                        }
-                        {
-                            y: '10',
-                            a: 2,
-                            b: 8
-                        }
-                        {
-                            y: '10',
-                            a: 2,
-                            b: 8
-                        }
-                    ],
-                    xkey: 'y',
-                    ykeys: ['a', 'b'],
-                    hideHover: 'auto',
-                    gridLineColor: '#eef0f2',
-                    resize: true,
-                    barSizeRatio: 0.4,
-                    labels: ['Successfull Orders', 'Failed Orders']
-                });
-            }
-
-            if ($("#morris-donut-example").length) {
-                Morris.Donut({
-                    element: 'morris-donut-example',
-                    resize: true,
-                    colors: ['#2e7ce4', '#00c2b2', '#df3554'],
-                    data: [{
-                            label: "Prod-1",
-                            value: 100
-                        },
-                        {
-                            label: 'Prod-2',
-                            value: 120
-                        },
-                        {
-                            label: 'Prod-3',
-                            value: 75
-                        }
-                    ]
-                });
-            }
-
-        });
-    </script>
 @endsection
